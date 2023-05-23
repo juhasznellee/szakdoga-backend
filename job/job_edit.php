adffbb342cea9b60bd7743639cb6b50dc2560e82
juhasznellee <juhasz.nelli16@gmail.com>
2023-05-23 23:38:35 +0200

    Cím módosítása

--- job/job_edit.php
+++ job/job_edit.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="..\editor.css">
-    <title>Munkák szerkesztése</title>
+    <title>Munkahelyek szerkesztése</title>
 </head>
 <body>
     <div class='ticketDesign'>
